a36c10a6d1b47172bedda4e37c05f3a5eb135858
sahaantara567 <sahaantara567@gmail.com>
2026-04-17 01:09:28 +0600

    add new changes

--- src/app/Cart/page.jsx
+++ src/app/Cart/page.jsx
@@ -4,28 +4,22 @@ import Cart from "@/components/Home/Cart";
 import React from "react";
 
 const CartPage = async () => {
-  const cartItems = await getCart();
-  const formattedItems =  cartItems.map((item)=>({
-    ...item,
-    _id: item._id.toString()
-  }));
+  try {
+    const cartItems = await getCart();
 
-  return (
-    <div className="max-w-7xl mx-auto px-6 py-10">
+    const formattedItems = cartItems.map(item => ({
+      ...item,
+      _id: item._id.toString()
+    }));
 
-      {/* Header */}
-      <div className="mb-8 border-b pb-6">
-        <h2 className="text-4xl font-bold text-gray-800 border-l-4 border-primary pl-4">
-        My Cart
-       </h2>
-
-       
-  </div>
-
-     <Cart cartItems={formattedItems}></Cart>
-      
-    </div>
-  );
+    return (
+      <div>
+        {/* your UI */}
+      </div>
+    );
+  } catch (error) {
+    console.error("Cart error:", error);
+    return <div>Error loading cart</div>;
+  }
 };
-
 export default CartPage;
