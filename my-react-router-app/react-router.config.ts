6cfbb3b9494ddcebbecf59f263f824eefddf02ad
sahaantara567 <sahaantara567@gmail.com>
2026-06-27 00:22:23 +0600

    Remove old React Router project

--- my-react-router-app/react-router.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-
-export default {
-  // Config options...
-  // server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
-} satisfies Config;
