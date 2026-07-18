c87ccae83856cad9fc1b7fdd391d0ce449e87138
sahaantara567 <sahaantara567@gmail.com>
2026-07-18 23:16:03 +0600

    add new changes

--- src/app/api/compare/route.js
+++ src/app/api/compare/route.js
@@ -1,21 +1,30 @@
-import { dbConnect, Collection } from "../../../app/lib/dbConnect";
 import { ObjectId } from "mongodb";
+import { dbConnect, Collection } from "../../lib/dbConnect";
 
 export async function GET(req) {
+  try {
+    const { searchParams } = new URL(req.url);
 
-  const { searchParams } = new URL(req.url);
-  const cottonType = searchParams.get("cottonType");
-  const id = searchParams.get("id");
+    const cottonType = searchParams.get("cottonType");
+    const id = searchParams.get("id");
 
-  const collection = await dbConnect(Collection.PRODUCTS);
+    const collection = await dbConnect(Collection.PRODUCTS);
 
-  const products = await collection
-    .find({
-      cottonType,
-      _id: { $ne: new ObjectId(id) }
-    })
-    .limit(5)
-    .toArray();
+    const query = { cottonType };
 
-  return Response.json(products);
+    if (id && ObjectId.isValid(id)) {
+      query._id = { $ne: new ObjectId(id) };
+    }
+
+    const products = await collection.find(query).limit(5).toArray();
+
+    return Response.json(products);
+  } catch (error) {
+    console.error(error);
+
+    return Response.json(
+      { error: error.message },
+      { status: 500 }
+    );
+  }
 }
